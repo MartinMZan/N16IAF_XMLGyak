e84e23582f271c5c57edeb922d50029e00740628
MartinMZan <79007717+MartinMZan@users.noreply.github.com>
2021-10-19 15:17:42 +0200

    Java part of the weekly exercise

---
+++ N16IAF_1019/SaxN16IAF/src/SaxN16IAF1019/SaxN16IAF.java
@@ -0,0 +1,21 @@
+package SaxN16IAF1019;
+
+import java.io.File;
+import java.io.IOException; 
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+
+
+public class SaxN16IAF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
