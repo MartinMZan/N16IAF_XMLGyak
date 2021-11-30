7afb6b20460cbe3b187ebb9a018754ec5d028adf
MartinMZan <79007717+MartinMZan@users.noreply.github.com>
2021-11-30 15:17:08 +0100

    Weekly Exercise

---
+++ N16IAF_1130/src/jsonN16IAF/ObjectN16IAF.java
@@ -0,0 +1,29 @@
+package jsonN16IAF;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class ObjectN16IAF {
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		// TODO Auto-generated method stub
+		
+		File inputFile = new File("input.txt");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(inputFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("student");
+        
+	}
+
+}
