c8851e005cd7d98e77089b2d660235453c0ba7ac
MartinMZan <zanmartin1998@gmail.com>
2021-12-02 11:29:02 +0100

    Exercise

--- N16IAF_1130/src/jsonN16IAF/ObjectN16IAF.java
+++ N16IAF_1130/src/jsonN16IAF/ObjectN16IAF.java
@@ -1,28 +1,22 @@
 package jsonN16IAF;
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
+import org.json.simple.JSONObject;
 
 public class ObjectN16IAF {
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		File inputFile = new File("input.txt");
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(inputFile);
-        doc.getDocumentElement().normalize();
-        NodeList nList = doc.getElementsByTagName("student");
+		JSONObject jsonObject = new JSONObject();
+		
+		jsonObject.put("Név", "ZMartin");
+		
+		jsonObject.put("Fizetes", "30í00");
+		
+		jsonObject.put("Kor", "23");
+		
+
+		System.out.println(jsonObject);
         
 	}
 
